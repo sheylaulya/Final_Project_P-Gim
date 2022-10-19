0e061238cf7be8da5bd0b7b99da17f287ccb1065
sheylaulya <sheylaulyaa@gmail.com>
2022-10-20 01:56:46 +0700

    login/regist,complete

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         $data = $connec->query(query: "INSERT INTO `user` VALUES ('NULL','$username','$email','$password')");
         if  ($data) {
             exit('Registration success...');
+            header('Location: .login');
         }else
             exit('failed, try again...');
     }
@@ -46,114 +47,123 @@ if(isset($_SESSION['loggedIN'])){
     <meta charset="utf-8">
     <title>Register</title>
     <link rel="stylesheet" href="style.css">
-</head>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet" 
+</head> 
 
-<body class="bg-[#0F1123]">
+    <body>
     <div class="main">
         <input type="checkbox" id="chk" aria-hidden="true">
 
-        <div class="signup">
-            <form autocomplete="off" action="index.php" method="post">
-                <label for="chk" aria-hidden="true">Sign up</label>
-                <input type="text" id="username" name="txt" placeholder="User name" required="">
-                <input type="email" id="email" name="email" placeholder="Email" required="">
-                <input type="password" id="password" name="pswd" placeholder="Password" required="">
-                <button id="regist">Sign up</button>
-            </form>
-        </div>
-
         <div class="login">
-            <form autocomplete="off" method="post" action="index.php">
+            <form autocomplete="off" method="post" action="index.php" class="form2">
                 <label for="chk" aria-hidden="true">Login</label>
+                <div class="loginnih"></div>
                 <input type="text" name="username" placeholder="username" id="username2" required="">
                 <input type="password" name="pswd" placeholder="Password" id="password2" required="">
                 <button id="login">Login</button>
             </form>
         </div>
 
+        <div class="signup">
+            <form autocomplete="off" action="index.php" method="post" class="form1">
+                <label for="chk" aria-hidden="true">Sign up</label>
+                <div class="inisignup">
+                    <input type="text" id="username" name="txt" placeholder="User name" required="">
+                    <input type="email" id="email" name="email" placeholder="Email" required="">
+                    <input type="password" id="password" name="pswd" placeholder="Password" required="">
+                    <button id="regist">Sign up</button>
+                </div>
+            </form>
+        </div>
+
+
+
         <p id="response"></p>
 
-        <!-- SCRIPT -->
-        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js"
-            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    </div>
 
-        <script type="text/javascript">
-            $(document).ready(function () {
-                $("#regist").on('click', function () {
-                    var email = $("#email").val();
-                    var username = $("#username").val();
-                    var password = $("#password").val();
+    <!-- SCRIPT -->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 
-
-                    if (email == "" || username == "" || password == "")
-                        swal({
-                            title: "Failed",
-                            text: "Please Check Your Inputs!",
-                            icon: "warning",
-                            button: "try again",
-                        });
-                    else {
-
-                        $.ajax({
-                            url: 'index.php',
-                            method: 'POST',
-                            data: {
-                                regist: 1,
-                                emailPHP: email,
-                                usernamePHP: username,
-                                passwordPHP: password
-                            },
-                            success: function (response) {
-                                $("#response").html(response);
-
-                            },
-                            dataType: 'text '
-                        })
-                    }
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $("#regist").on('click', function () {
+                var email = $("#email").val();
+                var username = $("#username").val();
+                var password = $("#password").val();
 
 
-                })
-            });
+                if (email == "" || username == "" || password == "")
+                    swal({
+                        title: "Failed",
+                        text: "Please Check Your Inputs!",
+                        icon: "warning",
+                        button: "ok",
 
-            $(document).ready(function () {
-                $("#login").on('click', function () {
-                    var username = $("#username2").val();
-                    var password = $("#password2").val();
+                    });
+                else {
 
-                    if (username == "" || password == "")
-                        swal({
-                            title: "Failed",
-                            text: "Please Check Your Inputs!",
-                            icon: "warning",
-                            button: "try again",
-                        });
-                    else {
+                    $.ajax({
+                        url: 'index.php',
+                        method: 'POST',
+                        data: {
+                            regist: 1,
+                            emailPHP: email,
+                            usernamePHP: username,
+                            passwordPHP: password
+                        },
+                        success: function (response) {
+                            $("#response").html(response);
 
-                        $.ajax({
-                            url: 'index.php',
-                            method: 'POST',
-                            data: {
-                                login: 1,
-                                usernamePHP: username,
-                                passwordPHP: password
-                            },
-                            success: function (response) {
-                                $("#response").html(response);
-
-
-                                if (response.indexOf('success') >= 0)
-                                    window.location = 'LS.php';
-                            },
-                            dataType: 'text '
-                        })
-                    }
+                        },
+                        dataType: 'text '
+                    })
+                }
 
 
-                })
             })
-        </script>
+        });
 
-</body>
+        $(document).ready(function () {
+            $("#login").on('click', function () {
+                var username = $("#username2").val();
+                var password = $("#password2").val();
+
+                if (username == "" || password == "")
+                    swal({
+                        title: "Failed",
+                        text: "Please Check Your Inputs!",
+                        icon: "warning",
+                        button: "try again",
+                    });
+                else {
+
+                    $.ajax({
+                        url: 'index.php',
+                        method: 'POST',
+                        data: {
+                            login: 1,
+                            usernamePHP: username,
+                            passwordPHP: password
+                        },
+                        success: function (response) {
+                            $("#response").html(response);
+
+
+                            if (response.indexOf('success') >= 0)
+                                window.location = 'LS.php';
+                        },
+                        dataType: 'text '
+                    })
+                }
+
+
+            })
+        })
+    </script>
+
+    </body>
 
 </html>
