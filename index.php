d4bb899f6efc0b3df5f18806a5acae80a13425f4
sheylaulya <sheylaulyaa@gmail.com>
2022-10-30 16:46:51 +0700

    REVISI

--- index.php
+++ index.php
@@ -13,67 +13,83 @@
    <link href='assets/img/icon-title.png' rel='shortcut icon'>
    <!-- css -->
    <link rel="stylesheet" href="assets/css/login.css">
-   <!-- font Quicksand -->
+   <!-- font -->
+   <link href="https://fonts.googleapis.com/css2?family=Rubik+Mono+One&family=Silkscreen:wght@400;700&display=swap"
+      rel="stylesheet">
+
    <link rel="preconnect" href="https://fonts.googleapis.com">
    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-   <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap" rel="stylesheet">
-   <!-- font silcscreen -->
-   <link rel="preconnect" href="https://fonts.googleapis.com">
-   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-   <link href="https://fonts.googleapis.com/css2?family=Silkscreen&display=swap" rel="stylesheet">
-   <!-- fontawsome -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-      integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-      crossorigin="anonymous" referrerpolicy="no-referrer" />
+   <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
+
    <!-- sweet alert -->
    <link rel="stylesheet" href="sweetalert2.min.css">
+
 </head>
 
 <body>
+   <nav>
+      <h1>Flappy Square</h1>
+      <ul>
+         <li class="active"></li>
+      </ul>
+      <div class="underline"></div>
+   </nav>
    <div class="container">
-      <div class="c1">
-
-         <div class="c11">
-            <h1 class="mainhead">PICK YOUR SPOT</h1>
-            <p class="mainp">Just click the buttons below to toggle between SignIn & SignUp</p>
-         </div>
-         <div id="left">
-            <h1 class="s1class"><span>SIGN</span><span class="su">IN</span>
-            </h1>
-         </div>
-         <div id="right">
-            <h1 class="s2class"><span>SIGN</span><span class="su">UP</span></h1>
-         </div>
+      <button id="btn">Click Here</button>
+   </div>
+   <div class="overlay">
+   </div>
+   <div class="main-popup">
+      <div class="popup-header">
+         <div id="popup-close-button"><a href="#"></a></div>
+         <ul>
+            <li><a href="#" id="sign-in">Sign In</a></li>
+            <li><a href="#" id="register">Register</a></li>
+         </ul>
       </div>
-      <div class="c2">
-         <form action="#" class="signup" id="SIGNUPFORM" method="POST">
-            <h1 class="signup1">SIGN UP</h1>
-            <br><br><br><br>
+      <!--.popup-header-->
+      <div class="popup-content">
+         <form class="register" id="SIGNUPFORM" method="POST" class="register">
             <input name="id" type="hidden" placeholder="id" class="id" id="id" />
+            <label for="fullname">Full Name </label>
             <input name="name" type="text" placeholder="Full Name" class="username" />
+            <label for="username">username </label>
             <input name="username" type="text" placeholder="Username" class="username" />
+            <label for="password">Password </label>
             <input name="password" type="password" placeholder="Password" class="username" />
-            <button class="btn">Sign Up</button>
+            <p class="check-mark">
+               <input type="checkbox" id="remember-me">
+               <label for="remember-me">Remember me</label>
+            </p>
+            <input type="submit" id="submit" value="Submit">
          </form>
-         <form action="#" class="signin" id="LoginForm" method="post">
-            <h1 class="signup1">SIGN IN</h1>
+
+         <form action="#" class="sign-in" id="LoginForm" method="post">
+            <label for="username">Username </label>
             <input type="text" name="Login_user" placeholder="username" id="name-user" autocomplete="off"
                class="username">
-            <input type="password" name="login_password" placeholder="Password" id="pw-login" autocomplete="off"
-               class="username">
-            <button class="btn">Get Started</button>
+            <label for="password">Password </label>
+            <input name="password" type="password" placeholder="Password" class="username" />
+            <p class="check-mark">
+               <input type="checkbox" id="accept-terms">
+               <label for="accept-terms">I agree to the <a href="#">Terms</a></label>
+            </p>
+            <input type="submit" id="submit" value="Submit">
          </form>
       </div>
+      <!--.popup-content-->
    </div>
-   </div>
+   <!--.main-popup-->
+
    <footer>
       <p> Copyright ©2022 All rights reserved | This template is made with by DivadanArya |</p>
       <div class="sosmed">
-        <a href=""><i class="fa-brands fa-facebook-f" style="color: black; opacity: .6; font-size: 18px;"></i></a>
-        <a href="https://www.instagram.com/divdannn_/"><i class="fa-brands fa-instagram" style="color: black; opacity: .6; font-size: 18px;"></i></a>
-        <a href=""><i class="fa-brands fa-twitter" style="color: black; opacity: .6; font-size: 18px;"></i></a>
+         <a href=""><i class="fa-brands fa-facebook-f" style="color: black; opacity: .6; font-size: 18px;"></i></a>
+         <a href="https://www.instagram.com/divdannn_/"><i class="fa-brands fa-instagram"
+               style="color: black; opacity: .6; font-size: 18px;"></i></a>
+         <a href=""><i class="fa-brands fa-twitter" style="color: black; opacity: .6; font-size: 18px;"></i></a>
       </div>
-    </footer>
+   </footer>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
       integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
       crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -81,6 +97,7 @@
    <script src="assets/javascript/animate.js"></script>
    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
    <script src="sweetalert2.min.js"></script>
+   <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 </body>
 
 </html>
